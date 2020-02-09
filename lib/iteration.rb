073d4735610e9d378e21e55047be51b7d47a0500
Carla D Sahagun <daniela.sahagun@hotmail.com>
2020-02-09 18:26:03 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -13,7 +13,7 @@ def join_ingredients(src)
     element_index = 0 # element counter
     element_results = [] # will save the elements of each array with every iteration
     while element_index < src[row_index].length do # will work till every element from each row is printed out or saved
-      element_results << src[row_index][element_index]
+      element_results << src[row_index][element_index] #we are saving each element into element_results
       element_index += 1
     end       #.push != << | push can take multiple arguments, << can only take one argument
     row_results << "I love #{element_results[0]} and #{element_results[1]} on my pizza"
@@ -59,30 +59,23 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-row_index = 0
-row_results = []
-sum = 0
-while row_index < src.length do
-  element_index = 0
-  element_results = []
-  while element_index < src[row_index].length do
-    element_results << src[row_index][element_index] # we save all the elements into element_results
-    if (element_results[0].even?) && (element_results[1].even?) #we have to filter all element_results into even numbers
-      sum += element_results[element_index]
-      p element_results[element_index]
+  row_index = 0
+  row_results = []
+  sum = 0
+  while row_index < src.length do
+    element_index = 0
+    element_results = []
+    while element_index < src[row_index].length do
+      element_results << src[row_index][element_index]
+      element_index += 1
     end
-    element_index += 1
+    if element_results[0].even? && element_results[1].even? #we have to filter all element_results into even numbers
+      sum += (element_results[0] + element_results[1])
+    end
+    row_results.push(element_results)
+    row_index +=1
   end
-  row_index += 1
-end
   p sum
 
 
 end
-
-
-src = [   #element0, #element1
-        [ 4, 2],              #row0
-        [ 12 , 9],          #row1
-        [ 5, 8]            #row2
-]
